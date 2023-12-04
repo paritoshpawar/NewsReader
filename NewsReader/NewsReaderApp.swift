ee0555f120218c3c2f0d67daa4cc16ccda8e7f17
pawarparitosh <Paritosh.Pawar@Amway.com>
2023-12-04 23:28:25 +0530

    Initial Commit

---
+++ NewsReader/NewsReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsReaderApp.swift
+//  NewsReader
+//
+//  Created by Mindstix on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
